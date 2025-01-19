f8a748f453c3d06fc0e8363f088cfc2c60e35a8a
afsar_raza <razaafsar9211@gmail.com>
2025-01-19 17:29:11 +0530

    Creation for Post REST API  and its associated repository, serviceImp and endpoint in the Controller.

---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,38 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        // convert DTO to entity
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post newPost = postRepository.save(post);
+
+        // convert entity to DTO
+        PostDto resposneDto = new PostDto();
+        resposneDto.setId(newPost.getId());
+        resposneDto.setTitle(newPost.getTitle());
+        resposneDto.setDescription(newPost.getDescription());
+        resposneDto.setContent(newPost.getContent());
+
+        return resposneDto;
+    }
+}
