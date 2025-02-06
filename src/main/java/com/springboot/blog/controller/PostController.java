43feb1cc5465ff863cbcae3cd367de0a69515e45
afsar_raza <razaafsar9211@gmail.com>
2025-02-06 23:07:03 +0530

    added the required dependency for validations of RestAPI's. And validated the createPost and updatePost REST apis

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -5,6 +5,7 @@ import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,7 @@ public class PostController {
 
     // create blog post rest api
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -46,7 +47,7 @@ public class PostController {
 
     // update post by id rest api
     @PutMapping("{id}")
-    public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto, @PathVariable(name = "id") long id){
+    public ResponseEntity<PostDto> updatePostById(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id){
         //return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
 
         PostDto responsePost = postService.updatePost(postDto, id);
